20c89001fe649592d7c4a862e879c9008f6b75e2
theo <tkleinen@gmail.com>
2016-09-01 16:25:59 +0100

    1st version after migration

--- gorinchem/urls.py
+++ gorinchem/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from views import HomeView
 
-urlpatterns = [
+admin.autodiscover()
+
+urlpatterns = patterns('delft.views',
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^data/', include('acacia.data.urls',namespace='acacia')),
+    url(r'^net/', include('acacia.meetnet.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
